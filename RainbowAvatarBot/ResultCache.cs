b8d7500d3517d9b49d5cde214d9d11892d29d9ca
Dubzer <dubzer@ya.ru>
2019-11-30 11:48:13 +0000

    Integrate caching results

---
+++ RainbowAvatarBot/ResultCache.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.Tracing;
+
+namespace RainbowAvatarBot {
+    internal class ResultCache {
+        private ConcurrentDictionary<(string fileID, string overlayName), string> dictionary = new ConcurrentDictionary<(string fileID, string overlayName), string>();
+
+        public bool TryAdd(string sourceId, string overlayName, string resultId) {
+            return dictionary.TryAdd((sourceId, overlayName), resultId);
+        }
+        
+        public bool TryGetValue(string sourceId, string overlayName, out string resultId) {
+            return dictionary.TryGetValue((sourceId, overlayName), out resultId);
+        }
+    }
+}
